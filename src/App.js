0c08a0cff4a15a36995df3f898afce5f64861429
abnaim33 <67532457+abnaim33@users.noreply.github.com>
2021-03-05 09:02:15 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import DisplayComponent from './Components/DisplayComponent';
 import BtnComponent from './Components/BtnComponent';
+
 import './App.css';
 
 function App() {
-  const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
+  const [time, setTime] = useState({ ms: 0, s: 0, m: 0, h: 0 });
   const [interv, setInterv] = useState();
   const [status, setStatus] = useState(0);
-  // Not started = 0
-  // started = 1
-  // stopped = 2
-
   const start = () => {
     run();
     setStatus(1);
@@ -20,20 +17,20 @@ function App() {
   var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
 
   const run = () => {
-    if(updatedM === 60){
+    if (updatedM === 60) {
       updatedH++;
       updatedM = 0;
     }
-    if(updatedS === 60){
+    if (updatedS === 60) {
       updatedM++;
       updatedS = 0;
     }
-    if(updatedMs === 100){
+    if (updatedMs === 100) {
       updatedS++;
       updatedMs = 0;
     }
     updatedMs++;
-    return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH});
+    return setTime({ ms: updatedMs, s: updatedS, m: updatedM, h: updatedH });
   };
 
   const stop = () => {
@@ -44,7 +41,7 @@ function App() {
   const reset = () => {
     clearInterval(interv);
     setStatus(0);
-    setTime({ms:0, s:0, m:0, h:0})
+    setTime({ ms: 0, s: 0, m: 0, h: 0 })
   };
 
   const resume = () => start();
@@ -52,12 +49,13 @@ function App() {
 
   return (
     <div className="main-section">
-     <div className="clock-holder">
-          <div className="stopwatch">
-               <DisplayComponent time={time}/>
-               <BtnComponent status={status} resume={resume} reset={reset} stop={stop} start={start}/>
-          </div>
-     </div>
+      <div className="clock-holder">
+        <div className="stopwatch">
+          <DisplayComponent time={time} />
+          <BtnComponent status={status} resume={resume} reset={reset} stop={stop} start={start} />
+        </div>
+        <h3>Created by  <a target="blank" className="portfolio_button" href="https://www.linkedin.com/in/md-naim-hossen-567679201">Naim Hossen</a></h3>
+      </div>
     </div>
   );
 }
